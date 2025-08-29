ad07cc108cd9fadcb255525fb3a7f220b58e530d
Morgan Hill <a@morganrhill.com>
2025-08-29 14:26:04 +0200

    Try mmapping DR file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use memmap::MmapOptions;
 use nova_vm::{
     ecmascript::{
         execution::{
@@ -8,12 +9,15 @@ use nova_vm::{
     },
     engine::context::Bindable,
 };
-use std::io::{Write, stderr, stdout};
-use std::ffi::CString;
+use std::{
+    fs::File,
+    io::{Write, stderr, stdout},
+    os::fd::{FromRawFd, RawFd},
+};
 
 const REPRL_CRFD: i32 = 100;
 const REPRL_CWFD: i32 = 101;
-const REPRL_DRFD: i32 = 102;
+const REPRL_DRFD: RawFd = 102;
 const REPRL_DWFD: i32 = 103;
 
 unsafe extern "C" {
@@ -21,7 +25,12 @@ unsafe extern "C" {
 }
 
 fn main() {
-    let mut buffer = Vec::new();
+    let dr_file = unsafe { File::from_raw_fd(REPRL_DRFD) };
+    let dr_mmap = unsafe {
+        MmapOptions::new()
+            .map(&dr_file)
+            .expect("couldn't map data read file")
+    };
 
     unsafe {
         libc::write(REPRL_CWFD, b"HELO".as_ptr() as *const libc::c_void, 4);
@@ -49,23 +58,10 @@ fn main() {
             assert_eq!(bytes_read, 8);
         }
         let script_len = u64::from_le_bytes(script_len_buf) as usize;
-        
-        buffer.clear();
-        buffer.resize(script_len + 1, 0);
 
-        unsafe {
-            let bytes_read = libc::read(
-                REPRL_DRFD,
-                buffer.as_mut_ptr() as *mut libc::c_void,
-                script_len,
-            );
-            assert_eq!(bytes_read, script_len as isize);
-        }
-
-        let script = CString::from_vec_with_nul(buffer.clone())
-            .expect("script not a CString")
-            .into_string()
-            .expect("not a valid rust string");
+        let script = str::from_utf8(&dr_mmap[..script_len])
+            .expect("invalid UTF8")
+            .to_string();
 
         let mut agent = GcAgent::new(
             Options {
@@ -84,7 +80,7 @@ fn main() {
                 Ok(_) => 0u32,
                 Err(e) => match e.value() {
                     Value::Integer(i) => ((i.into_i64() as u32) & 0xFF) << 8,
-                    _ => (1u32 & 0xFF) << 8
+                    _ => (1u32 & 0xFF) << 8,
                 },
             };
 
